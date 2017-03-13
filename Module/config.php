1461141bda1687e8da95da69417429d714c3c0d4
Heekei <heekei@foxmail.com>
2017-03-13 11:35:54 +0800

    config 2

---
+++ Module/config.php
@@ -0,0 +1,15 @@
+<?php 
+     //数据库服务器地址
+    //  $GLOBALS["server_URL"] = "guxfs.gotoftp4.com";
+     $GLOBALS["server_URL"] = "115.28.247.208";
+     //数据库登录名
+     $GLOBALS["user_name"] = "root";
+     // 数据库密码
+     $GLOBALS["pwd"] = "123456";
+     // 数据库名称
+     $GLOBALS["db_name"] = "hos";
+     //设置表名前缀
+     $GLOBALS["pre_name"] = "hos";
+     //产品表名
+     $GLOBALS["users"] = $pre_name . "_users";
+ ?>
